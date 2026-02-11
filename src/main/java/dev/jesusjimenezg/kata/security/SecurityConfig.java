e8f8cd92bf120515ac95fbe2a4760b7907f11e3a
Jesus Jimenez <jesusalfonsojg7@gmail.com>
2026-02-11 12:31:48 -0400

    Initialize OpenAPI and Swagger UI

--- src/main/java/dev/jesusjimenezg/kata/security/SecurityConfig.java
+++ src/main/java/dev/jesusjimenezg/kata/security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/register", "/api/auth/login", "/api/auth/refresh").permitAll()
+                    .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
